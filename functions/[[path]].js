56fb414922c0fbe1556fb1ec4bfcedbe4d073378
Quasvx <mathiasza422@gmail.com>
2026-05-27 18:18:19 -0500

    Update redirection logic in onRequest function

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,13 +1,13 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  // Si ya están en el dominio nuevo, NO redirigir (evita el bucle de payaso)
+  // Si ya están en el dominio definitivo, dejamos que pase a los archivos estáticos
   if (url.hostname === 'rankyfnf.cc.cd') {
-    return next(); 
+    return context.next();
   }
 
-  // Si vienen de cualquier otro lado (como rankyfnf.pages.dev), los mandamos al nuevo
+  // Si vienen de pages.dev, los mandamos al nuevo con sus rutas y queries intactas
   const targetURL = `https://rankyfnf.cc.cd${url.pathname}${url.search}`;
-
+  
   return Response.redirect(targetURL, 301);
 }
