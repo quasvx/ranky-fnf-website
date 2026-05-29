b2a87126b110879fe768f282960ef40e65ed7393
Quasvx <mathiasza422@gmail.com>
2026-05-29 18:12:18 -0500

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,12 +2,12 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
 
   // Si ya están en el dominio definitivo, dejamos que pase a los archivos estáticos
-  if (url.hostname === 'rankyfnf.cc.cd') {
+  if (url.hostname === 'www.rankyfnf.cc.cd') {
     return context.next();
   }
 
   // Si vienen de pages.dev, los mandamos al nuevo con sus rutas y queries intactas
-  const targetURL = `https://rankyfnf.cc.cd${url.pathname}${url.search}`;
+  const targetURL = `https://www.rankyfnf.cc.cd${url.pathname}${url.search}`;
   
   return Response.redirect(targetURL, 301);
 }
